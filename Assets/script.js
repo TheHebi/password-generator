f0f6291a7c6bcb2d43a4ed9dc3d653c0771c6528
TheHebi <nturcotte8@live.com>
2021-06-22 19:35:50 -0700

    created character pool for password generation, working generator if all options choose ok

--- Assets/script.js
+++ Assets/script.js
@@ -2,18 +2,46 @@
 // after pop ups are finished, clicking "Generate Password" button will print a randomized password in the box displaying "Your Secure Password"
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// password length pop up
-var passLength = prompt("How many characters do you want?")
 
-function generatePassword(){
-  // return "123" / line added to check script.js linked to html correctly
-}
+// // password length pop up
+let passLength = prompt("How many characters do you want?");
+
+// // setting a minimum and maximum character limit for password length
+// if ((passLength < 10)) {
+//   alert("Password must be at least 10 characters long");
+//   passLength = prompt("How many Characters do you want?")
+// }else if (passLength > 150) {
+//   alert("Password must be less than 150 characters long");
+//   passLength = prompt("How many characters do you want?");
+// }else {
+//   alert(`You have chosen a ${passLength} long password`)
+// }
 
 // pop up windows with (y/n) answers
-alert("Use Uppercase Letters?")
-window.confirm("Use Lowercase Letters?")
-window.confirm("Use Numbers?")
-window.confirm("Use Special Characters?")
+let upper = window.confirm("Use Uppercase Letters?");
+let lower = window.confirm("Use Lowercase Letters?");
+let numbs = window.confirm("Use Numbers?");
+let specs = window.confirm("Use Special Characters?");
+
+//character pool for password generation
+let security = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+let result = "";
+
+// password generation function
+function generatePassword() {
+  // return "123" / line added to check script.js linked to html correctly
+  if (upper === lower === numbs === specs === true){
+    for (let i = 0; i < passLength; i++) {
+      result += security.charAt(Math.floor(Math.random() * security.length));
+    }
+    return result;
+  }
+  // testing function to pull random character from pool
+  // for (let i = 0; i < passLength; i++) {
+  //   result += security.charAt(Math.floor(Math.random() * passLength));
+  // }
+  // return result;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -21,7 +49,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
