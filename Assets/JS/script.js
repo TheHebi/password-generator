6403de1984a421ce63dc2ece51b6ca25916e5105
TheHebi <nturcotte8@live.com>
2021-06-23 15:40:46 -0700

    moved prompts to function on click of generate button instead of on load

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,7 +4,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // // password length pop up
-let passLength = prompt("How many characters do you want?");
+// let passLength = prompt("How many characters do you want?");
 
 // setting a minimum and maximum character limit for password length / initial attempt. friend reccomended looking into a recursive or dowhile
 // if ((passLength < 10)) {
@@ -17,11 +17,11 @@ let passLength = prompt("How many characters do you want?");
 //   alert(`You have chosen a ${passLength} long password`)
 // }
 
-// pop up windows with (y/n) answers
-let upper = window.confirm("Use Uppercase Letters?");
-let lower = window.confirm("Use Lowercase Letters?");
-let numbs = window.confirm("Use Numbers?");
-let specs = window.confirm("Use Special Characters?");
+// pop up windows with (true/false) answers
+// let upper = confirm("Use Uppercase Letters?");
+// let lower = confirm("Use Lowercase Letters?");
+// let numbs = confirm("Use Numbers?");
+// let specs = confirm("Use Special Characters?");
 
 //character pool for password generation
 // let secure ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"; / variable used to test initial randomizer / decided to use multiple variable strings and concat for a simpler approach to generation
@@ -32,18 +32,24 @@ let specialChars = "!@#$%^&*()";
 let pwPool = "";
 let result = "";
 
-// used to check true/false values of confirm pop ups
-console.log(upper)
-console.log(lower)
-console.log(numbs)
-console.log(specs)
-
 // password generation function
 // Initially i was planning on using multiple else if statements to check each individual variation for trues and falses.
 // I wasn't sure if i could nest if statments into an else statemnt and get the results I was looking for.
 // After some failed attempts I got the if(upper) statement to work and from there it was just copying the statement and changing the variables to the corresponding variables for each new if statement.
 // The if/else statment now works correctly for each variable being either true or false
 function generatePassword() {
+  let passLength = prompt("How many characters do you want?");
+  let upper = confirm("Use Uppercase Letters?");
+  let lower = confirm("Use Lowercase Letters?");
+  let numbs = confirm("Use Numbers?");
+  let specs = confirm("Use Special Characters?");
+
+  // used to check true/false values of confirm pop ups
+  // console.log(upper);
+  // console.log(lower);
+  // console.log(numbs);
+  // console.log(specs);
+
   // return "123" / line added to check script.js linked to html correctly
 
   // initial code for testing returns
@@ -62,25 +68,25 @@ function generatePassword() {
   if (!upper && !lower && !numbs && !specs) {
     result += "No password variables selected";
     return result;
-  }else {
+  } else {
     // if upper is true
     if (upper) {
-      pwPool = pwPool + capLetters
+      pwPool = pwPool + capLetters;
     }
     // if lower is true
-    if (lower){
-      pwPool = pwPool + letters
+    if (lower) {
+      pwPool = pwPool + letters;
     }
     // if numbs is true
-    if(numbs){
-      pwPool = pwPool + numbers
+    if (numbs) {
+      pwPool = pwPool + numbers;
     }
     // if specs is true
-    if(specs){
-      pwPool = pwPool + specialChars
+    if (specs) {
+      pwPool = pwPool + specialChars;
     }
-    for ( let i = 0; i < passLength; i++){
-    result += pwPool.charAt(Math.floor(Math.random() * pwPool.length));
+    for (let i = 0; i < passLength; i++) {
+      result += pwPool.charAt(Math.floor(Math.random() * pwPool.length));
     }
     return result;
   }
